82585fbd89f9b0b1f523ffa34482e8112b1b10ca
Sean Kelley <s.kelley27@gmail.com>
2014-03-28 13:53:41 -0500

    Added comments

--- P15/latticePaths.rb
+++ P15/latticePaths.rb
@@ -1,15 +1,20 @@
 #!/usr/bin/ruby
 
+# input the size of grid
 def grid(input)
+	#initialize arrays
 	last = Array.new(input+1){1}
 	current =  Array.new(input+1)
+	#for every square over
 	for i in 1..input
 		current[0] = 1
+		#for every square down
 		for i in 1..input
 			current[i] = last[i]+current[i-1]	
 		end
 		last = current
 	end
+	#return the final number
 	return current[input]	
 end
 
