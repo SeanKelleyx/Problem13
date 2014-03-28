3266319aee227c48b3a951dad3f48efce29ee79a
Sean Kelley <s.kelley27@gmail.com>
2014-03-28 14:14:10 -0500

    Added comments

--- P16/powerDigitSum.rb
+++ P16/powerDigitSum.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/ruby
 
+#function returns x to the power of y
 def powerDigit(x,y)
 	return x**y
 end
+
+#returns the sum of all digits in the input number
 def digitSum(input)
+	#initialize total
 	total = 0
+	#initialize input as string
 	input = input.to_s
+	#for every digit...
 	for i in 0..input.length-1
+		#add digit as int to total
 		total += input[i].to_i
 	end
+	#return the total
 	return total
 end
 
