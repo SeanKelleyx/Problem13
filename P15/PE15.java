1a1d132290e5c337de3fe2a4e913759d0f945291
Sean Kelley <s.kelley27@gmail.com>
2014-03-31 10:59:19 -0500

    Added comments for java solution

--- P15/PE15.java
+++ P15/PE15.java
@@ -1,30 +1,31 @@
 import java.util.Arrays;
 
-/**
- *
- * @author kelleys
- */
+
 public class PE15 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // call the  grid function with the size of grid
         System.out.println("The answer is " + grid(20));
     }
-    
+    //input is the size of grid
     public static long grid(int input){
+	//initialize arrays and fill last with 1's
         long[] last = new long[input+1];
         Arrays.fill(last, 1);
         long[] current = new long[input+1];
+	//for every square over
         for (int i = 1; i <= input; i++) {
             current[0] = 1;
+	    //for every square down
             for (int j = 1; j <= input; j++) {
                 current[j] = last[j]+current[j-1];
             }
             last = current;
         }
+ 	//return the final solution possible number of paths
         return current[input];
     }
     
