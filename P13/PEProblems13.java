da106fa8b060fbdffedf3d7341125fb199ca63f5
Sean Kelley <s.kelley27@gmail.com>
2014-03-31 10:19:52 -0500

    added comments on java solution

--- P13/PEProblems13.java
+++ P13/PEProblems13.java
@@ -15,15 +15,20 @@ public class PEProblems13 {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
+	// open and read file line by line
         BigInteger total = new BigInteger("0");
         FileInputStream fstream = new FileInputStream("numbers.txt");
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         String line;
         while ((line = br.readLine()) != null) {
+	    //remove line return chars
             String s2 = line.replaceAll("[\n\r]", "");
+	   // make lin e a big integer
             BigInteger plus = new BigInteger(s2);
+	    //add to tota;
             total = total.add(plus);
         }
+        //print total of ints
         System.out.println(total);
     }
     
