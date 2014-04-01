25d99d28719f049a9c17a4eb5f0eb0240b9a1a36
Sean Kelley <s.kelley27@gmail.com>
2014-04-01 13:07:03 -0500

    Added comments to ruby solution and added a function to keep code more modual

--- P21/amicableNums.rb
+++ P21/amicableNums.rb
@@ -1,26 +1,44 @@
 #!/usr/bin/ruby
 
-def amicable(num)
+# returns the sum of proper divisors of a number
+def sumDiv(num)
+	#initialize total
 	total = 0
+	#for every number less than num
 	for i in 1..num-1
+		#if i is a proper divisor
 		if isPropDiv(num, i)
+			#add i to total
 			total += i
 		end
 	end
+	# return total
 	return total
 end
 
+#returns true if div is a proper divisor of base false if not
 def isPropDiv(base, div)
 	return base%div == 0
 end
 
+#return true if given number is an amicable num false if not
+def isAmicable(num)
+	return sumDiv(sumDiv(num)) == num && sumDiv(num) != num
+end
+
+#returns the sum of all amicable pairs under a number
 def sumAmi(num)
+	#initialize total to 0
 	total = 0
+	#for every number under num
 	for i in 1 .. num-1
-		if amicable(amicable(i)) == i && amicable(i) != i
+		#if it is an amicable number
+		if isAmicable(i)
+			#add i to total
 			total += i
 		end 
 	end
+	#return total
 	return total
 end
 
