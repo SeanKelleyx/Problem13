d26ae614a268a00b15e3e77a76c27b2cb11c6281
Sean Kelley <s.kelley27@gmail.com>
2014-04-01 12:31:44 -0500

    Added comments to fact function in ruby solution for problem 20

--- P20/factDigitSum.rb
+++ P20/factDigitSum.rb
@@ -15,12 +15,17 @@ def digitSum(input)
         return total
 end
 
+# returns the factorial of number x
 def fact(x)
-	total = 1
+	#initialize total to 1
+	fact = 1
+	#for each number in x
 	for i in 1..x
-		total *= i
+		#multiply total by number
+		fact *= i
 	end
-	return total
+	#return factorial
+	return fact
 end
 
 puts digitSum(fact(100))
