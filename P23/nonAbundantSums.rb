ff5d3a58c11e5225360115fa1facfdde51382ea6
Sean Kelley <s.kelley27@gmail.com>
2014-04-02 15:11:50 -0500

    Tried to reduce execution time, cut down slightly

--- P23/nonAbundantSums.rb
+++ P23/nonAbundantSums.rb
@@ -1,55 +1,29 @@
 #! /usr/bin/ruby
 
-#return the sum of a numbers divisors
-def getDivSums(n)
-	#initialize total
-	total = 0
-	#for every number less than n
-	for i in 1..n-1
-		#if i is a divisor
-		if n%i == 0
-			#add divisor to total
-			total += i
-		end
-	end
-	#return total
-	return total			
-end
-
-#return array of abundant numbers under a limit
-def getAbundants(cap)
-	#init array
+#return array from csv file
+def getAbundants()
+	#open file
+	f = File.open("abundants.csv", "r")
+	#initialize array
 	abs = []
-	#for every number under cap
-	for i in 1..cap-1
-		#if number is abundant
-		if isAbundant(i)
-			#add number to abs array
-			abs << i
-		end
+	f.each_line do |line|
+		#remove line escape
+		line = line.strip
+		#split into array at ,'s
+		abs = line.split(',').map(&:to_i)
 	end
-	#return abs
+	#close file
+	f.close
+	#return array
 	return abs
-end
-
-#return true if number is abundant, false if not
-def isAbundant(n)
-	return getDivSums(n) > n
-end
-
-#return true if number is in array, false if not
-def isInArray(n, arr)
-	return arr.include? n
 end
 
 #return true if n cannot be sum of two abundant nums false if not
 def nonSumAb(n, abs)
-	#init smallAbs
-	smallAbs = abs.reject {|x| x > n}
-	#for evey ab in smallAbs
-	for i in smallAbs
+	#for evey ab in abs
+	for i in abs
 		#if it can be added to another abundant to get n
-		if isInArray(n-i, smallAbs)
+		if abs.include? n-i
 			#return value false
 			return false	
 		end
@@ -63,9 +37,11 @@ def totNonAbSums(cap)
 	#init total
 	total = 0
 	#init abs
-	abs = getAbundants(cap)
+	abs = getAbundants()
 	#for every number up to cap
-	for i in 1..cap
+	for i in cap.downto(1)
+		#reduce abs by removing numbers higher than i
+		abs = abs.reject {|x| x > i}
 		#if i is not a sum of abundants
 		if nonSumAb(i, abs)
 			#add i to total
