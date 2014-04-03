086f3f4f2a3230c4bb46c5d300722e0cfa972d7d
Sean Kelley <s.kelley27@gmail.com>
2014-04-03 15:51:48 -0500

    added comments

--- P24/lexiPerms.rb
+++ P24/lexiPerms.rb
@@ -1,8 +1,10 @@
 #! /usr/bin/ruby
 
+#return array of all permutations (in lexi order)
 def getPerms(x, y)
 	perms = (x..y).to_a.permutation.map(&:join)
 	return perms
 end
 
+#print the 1000000th permutation
 puts getPerms(0, 9)[999999]
