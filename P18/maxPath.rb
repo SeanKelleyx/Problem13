93474d5986f70ba1b5faf1119b9079031bb062ac
Sean Kelley <s.kelley27@gmail.com>
2014-04-04 13:15:53 -0500

    Added ruby solution for problem 18

---
+++ P18/maxPath.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/ruby
+
+def getTri()
+	f=File.open("tri.csv", "r")
+	tri = []
+	f.each_line do |line|
+		line.strip!
+		tri << line.split(',').map(&:to_i)
+	end
+	f.close
+	return tri
+end
+
+def getSumsByRow(tri, row)
+	for i in 0..tri[row].length-1
+		tri[row][i] += [tri[row+1][i], tri[row+1][i+1]].max
+	end
+	if tri[row].length == 1
+		return tri[row][0]
+	else
+		return getSumsByRow(tri, row-1)
+	end
+end
+
+tri = getTri()
+puts getSumsByRow(tri, tri.length-2)
