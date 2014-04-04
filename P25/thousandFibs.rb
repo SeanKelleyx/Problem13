fae1b010edd8698f4e437f3390c44cc466221c1c
Sean Kelley <s.kelley27@gmail.com>
2014-04-04 12:23:20 -0500

    Added comments to solution to problem 22

--- P25/thousandFibs.rb
+++ P25/thousandFibs.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/ruby
 
+# retrn the count of which fib. number is the first to be 1000 digits long
 def thousandDig()
+	#init count
 	count = 1
+	#init numbers for fibonacci sequence
 	current, last = 1, 0
+	#while less than 100 dig long
 	while current.to_s.length < 1000
+		# set last to current and curren to nex fib
 		last, current = current, last + current
+		#increment counter
 		count +=1
 	end
+	#return counter
 	return count
 end
 
