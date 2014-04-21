105bc41d41e90251e15ab0c4871a22453b9c610b
Sean Kelley <s.kelley27@gmail.com>
2014-04-21 14:53:58 -0500

    Finalized solution to P26

--- P26/longestRecip.rb
+++ P26/longestRecip.rb
@@ -1,24 +1,6 @@
 #! /usr/bin/ruby
 
-sequenceLength = 0
-
-for x in 1000.downto(1)
-	if (sequenceLength >= x)
-		break
-	end
-	foundRemainders = [x]
-	value =1
-	position = 0
-	while (foundRemainders[value] == 0 && value != 0)
-		foundRemainders[value] = position
-		value *= 10
-		value %= x
-		position += 1
-	end
-	if (position - foundRemainders[value] > sequenceLength)
-		sequenceLength  = position - foundRemainders[value]
-	end
-end
-puts sequenceLength
-	
+puts (0..1000).map{|d| 
+(	1..d).detect(lambda{0}) {|t| (10**t% d) ==1}
+}.each_with_index.max[1]		
 	
