ec7616077c34de413dacdf8b5657885fb32207cd
Sean Kelley <s.kelley27@gmail.com>
2014-04-22 09:08:03 -0500

    Added comments to solution.

--- P29/DistinctPowers.rb
+++ P29/DistinctPowers.rb
@@ -1,10 +1,16 @@
 #! /usr/bin/ruby
 
+#set array
 array = []
+#for every a
 for a in 2..100
+	#for every b
 	for  b in 2..100
+		#add term to array
 		array << a**b
 	end
 end
+#remove duplicate terms
 array = array.uniq
+#print the count of terms in the array
 puts array.count
