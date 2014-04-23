b32fc82fac701da257c5616d21a8b6d25e4be4af
Sean Kelley <s.kelley27@gmail.com>
2014-04-23 12:07:38 -0500

    Added fixed solution to problem 31

--- P31/coinSums.rb
+++ P31/coinSums.rb
@@ -3,10 +3,14 @@
 def makeArr()
 	coins = [1,2,5,10,20,50,100,200]
 	arr = []
-	for x in 1..200
-		arr += coins.repeated_combination(x).to_a
+	total = 0
+	for x in 1..30
+		#arr = coins.repeated_combination(x).to_a
+		#total += getCount(getTotals(arr))
+		arr += coins.repeated_combination(x).inject {|sum, n| sum +n}
+		puts "after x = #{x}, the total is #{total}"
 	end
-	return arr
+	return total
 end
 
 def getTotals(arr)
@@ -20,10 +24,11 @@ def getTotals(arr)
 			tot200 << total 
 		end
 	end
+	return tot200
 end
 
 def getCount(arr)
 	return arr.count
 end
 
-getCount(getTotals(makeArr()))
+puts makeArr()
