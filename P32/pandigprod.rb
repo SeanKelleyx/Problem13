53a9f2b1d72a46165ebec47a245bc746997938d7
Sean Kelley <s.kelley27@gmail.com>
2014-04-25 11:35:42 -0500

    Added total array function and streamlined isPan function

--- P32/pandigprod.rb
+++ P32/pandigprod.rb
@@ -2,25 +2,18 @@
 
 def isPan (multiplicand, multiplier, product)
 	arr = multiplicand.to_s.chars.map(&:to_i)
-	arr << multiplier.to_s.chars.map(&:to_i)
-	arr << product.to_s.chars.map(&:to_i)
-	arr.sort!
-	y = 1
-	for x in arr
-		if x == y
-			y+=1
-		else 
-			return false
-		end
-	end
-	return true 
+	arr += multiplier.to_s.chars.map(&:to_i)
+	arr += product.to_s.chars.map(&:to_i)
+	return arr.sort == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 end
 
 def run (topend)
 end
 
 def getMults(product)
+	
 end
 
 def total(prodsArr)
+	return prodsArr.inject{|sum,x| sum + x }
 end
