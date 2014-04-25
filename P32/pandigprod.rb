f93ee47a7e013b8db3b6cd784dca37c8f6a11564
Sean Kelley <s.kelley27@gmail.com>
2014-04-25 12:34:41 -0500

    finalized solution to problem 32

--- P32/pandigprod.rb
+++ P32/pandigprod.rb
@@ -7,13 +7,25 @@ def isPan (multiplicand, multiplier, product)
 	return arr.sort == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 end
 
-def run (topend)
-end
-
-def getMults(product)
-	
+def run ()
+	prodsArr = []
+	for x in 100..99999
+		for y in 2..Math.sqrt(x).floor
+			next if x%y != 0
+			z = x/y
+			total = (Math.log10(x).to_i+1)+(Math.log10(y).to_i+1)+(Math.log10(z).to_i+1)
+			next if total != 9
+			if isPan(y, z, x)
+				prodsArr << x
+				break
+			end	
+		end
+	end
+	puts total(prodsArr)
 end
 
 def total(prodsArr)
 	return prodsArr.inject{|sum,x| sum + x }
 end
+
+run()
