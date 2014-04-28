319d27bdfc4e37327a649002be537bc11c86c3d1
Sean Kelley <s.kelley27@gmail.com>
2014-04-28 13:00:57 -0500

    Still working on a solution

--- P35/CircularPrimes.rb
+++ P35/CircularPrimes.rb
@@ -1,12 +1,20 @@
 #! /usr/bin/ruby
 
 def getCirNums(x)
-	x.to_s.chars.map(&:to_i)
+	x1 = x.to_s.chars.map(&:to_i)
+	cirs = [x]
+	for y in 1..x1.length-1
+		x1 << x1[0]
+		x1 = x1.shift
+		temp =  x1.inject{|a,i| a*10 + i}
+		cirs << temp
+	end
+	return cirs
 end
 
 def isPrime(x)
-	for y in 2..Math.sqrt(x).floor
-		if x%y != 0
+	for y in 2..Math.sqrt(x).ceil
+		if x%y == 0
 			return false
 		end
 	end
@@ -23,3 +31,17 @@ def getPrimes
 	return arr
 end
 
+def checkPrimes()
+	count = 4
+	primes = getPrimes()
+	for x in primes
+		cirs = getCirNums(x)
+		if (cirs - primes).length == 0
+			count += cirs.length
+			primes = primes - cirs
+		end
+	end
+	puts count
+end
+
+checkPrimes()
