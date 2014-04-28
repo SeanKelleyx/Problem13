3b98369010b0923b7a3715426bef603bc04650fb
Sean Kelley <s.kelley27@gmail.com>
2014-04-28 15:11:35 -0500

    Finalized solution for problem 35

--- P36/twoBasePalindrome.rb
+++ P36/twoBasePalindrome.rb
@@ -7,4 +7,24 @@ def isPalindrome(x,y)
 	return false
 end
 
+def toBin(x)
+	return x.to_s(2).to_i
+end
 
+def getReverse(x)
+	return x.to_s.reverse.to_i
+end
+
+def run()
+	total = 0
+	for i in 1..1000000
+		ib = toBin(i)
+		next if ib%10 == 0
+		if i == getReverse(i) && ib == getReverse(ib)
+			total += i
+		end
+	end
+	puts total
+end 
+
+run()
