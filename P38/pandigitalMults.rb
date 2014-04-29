83f19826d07f3a36504e448532dabd438a007fb3
Sean Kelley <s.kelley27@gmail.com>
2014-04-29 09:33:47 -0500

    Added main run function for P38

--- P38/pandigitalMults.rb
+++ P38/pandigitalMults.rb
@@ -1,6 +1,22 @@
 #! /usr/bin/ruby
 
 def run()
+	best = 0
+	for x in 1..9999
+		test = ""
+		i = 1
+		while test.length < 9
+			test += xtos(x, i)
+			i += 1
+		end
+		if test.length = 9 && isPandigital(test) && test.to_i > best
+			best = test.to_i
+		end
+	end
+	return best
+end
 
+def xtos(x, y)
+	return x*y.to_s
 end
 
