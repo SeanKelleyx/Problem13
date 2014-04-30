7e5f7e936b81ebe93dd368864165bf3b99ad73d5
Sean Kelley <s.kelley27@gmail.com>
2014-04-30 16:23:57 -0500

    Completed solution, needs trimming to speed up: next if over perimeter.. etc

--- P39/intRightTri.rb
+++ P39/intRightTri.rb
@@ -4,8 +4,8 @@ def isEven?(x)
 	return Math.sqrt(x)%1 == 0
 end
 
-def equalsPerimeter?(anb, c, perim)
-	return anb + c == perim
+def equalsPerim?(a, b, c, perim)
+	return a + b + c == perim
 end
 
 def getCsq(a, b)
@@ -16,4 +16,33 @@ def getRt(x)
 	return Math.sqrt(x)
 end
 
+def getNumSols(perim)
+	total = 0 
+	for a in 1..(perim-1)
+		for b in 1..(a-1)
+			next if !isEven?(getCsq(a, b))
+			c = getRt(getCsq(a, b))
+			if equalsPerim?(a,b,c,perim)
+				total += 1
+			end
+		end
+	end
+#	puts "#{perim} -- #{total}"
+	return total
+end
+
+def getBestPerim(topend)
+	best = 0
+	bestSols = 0
+	for x in 3..topend
+		thisSol = getNumSols(x)
+		if thisSol > bestSols
+			best = x
+			bestSols = thisSol
+		end
+	end
+	return best
+end
+
+puts getBestPerim(1000)
 
