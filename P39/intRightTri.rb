fec6052c674e2bc2de25f8635441087979126227
Sean Kelley <s.kelley27@gmail.com>
2014-04-30 15:49:32 -0500

    Working on solution for P39

---
+++ P39/intRightTri.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/ruby
+
+def isEven?(x)
+	return Math.sqrt(x)%1 == 0
+end
+
+def equalsPerimeter?(anb, c, perim)
+	return anb + c == perim
+end
+
+def getCsq(a, b)
+	return a**2 + b**2
+end
+
+def getRt(x)
+	return Math.sqrt(x)
+end
+
+
